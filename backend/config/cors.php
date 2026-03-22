942dd33904785babd17ee38530d6944f25533582
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2026-03-22 14:47:54 +0100

    bugfix

---
+++ backend/config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['https://afstudeerproject-jarnoleuckx.onrender.com'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
