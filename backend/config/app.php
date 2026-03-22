d75517f3374123ae5c381cffefed972bd813fee7
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2026-03-22 17:19:00 +0100

    bugfix

--- backend/config/app.php
+++ backend/config/app.php
@@ -124,4 +124,36 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+        /* ...and other Laravel providers... */
+        Barryvdh\DomPDF\ServiceProvider::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+
+    'aliases' => [
+        /* ...and other aliases... */
+        'Pdf' => Barryvdh\DomPDF\Facade\Pdf::class,
+    ],
+
 ];
