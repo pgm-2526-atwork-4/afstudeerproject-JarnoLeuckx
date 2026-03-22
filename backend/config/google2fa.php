d3d60769907f0ee574b2f9a04e4e5e11ac3a39b9
JarnoLeuckx <jarno.leuckx@student.arteveldehs.be>
2026-03-22 15:34:50 +0100

    update

--- backend/config/google2fa.php
+++ backend/config/google2fa.php
@@ -9,8 +9,6 @@ return [
 
     /*
      * Lifetime in minutes.
-     *
-     * In case you need your users to be asked for a new one time passwords from time to time.
      */
     'lifetime' => env('OTP_LIFETIME', 0), // 0 = eternal
 
@@ -74,10 +72,8 @@ return [
     'throw_exceptions' => env('OTP_THROW_EXCEPTION', true),
 
     /*
-     * Which image backend to use for generating QR codes?
-     *
-     * Supports imagemagick, svg and eps
+     * QR code image backend
      */
-    'qrcode_image_backend' => \PragmaRX\Google2FALaravel\Support\Constants::QRCODE_IMAGE_BACKEND_SVG,
+    'qrcode_image_backend' => 'svg',
 
 ];
